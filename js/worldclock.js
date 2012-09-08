78dedd9a1563d62bb04234182b1076c96e26d030
Benedikt Budig <benedikt.budig@binaerpiloten.de>
2012-09-07 21:55:19 -0400

    Uhr verschönert

--- js/worldclock.js
+++ js/worldclock.js
@@ -18,6 +18,9 @@ function tickWorldclock(div) {
 	var offset = $(div).attr('data-offset');
 	var time = new Date();
 	time.setUTCMinutes(time.getUTCMinutes() + new Date().getTimezoneOffset() + parseInt(offset));
-	$(div).html(time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds());
+	currentHour = (time.getHours() < 10 ? "0" : "") + time.getHours();
+	currentMinute = (time.getMinutes() < 10 ? "0" : "") + time.getMinutes();
+	currentSecond = (time.getSeconds() < 10 ? "0" : "") + time.getSeconds();
+	$(div).html(currentHour + '<span class="seperator">:</span>' + currentMinute + '<span class="seperator">:</span>' + currentSecond);
 	setTimeout('tickWorldclock("' + div + '")', 1000);
 }
