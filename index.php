9eacf94c4ef79b9439ce19ff4cf444e876d35c2a
Nils Wisiol <info@nils-wisiol.de>
2012-11-05 11:28:04 -0500

    buffalo Winterzeit

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
         initDiashow('<?php if (file_exists('diashowpath.txt')) { include('diashowpath.txt'); } else { echo "img/diashow-default.png"; } ?>', '#diashow', 2, 60 * 1000);
         
         // Weltuhr initialisieren
-        initWorldclock('#worldclock-edt', -4 * 60); // Buffalo
+        initWorldclock('#worldclock-est', -5 * 60); // Buffalo, Winterzeit
         initWorldclock('#worldclock-cet', 1 * 60); // Würzburg, Winterzeit
         initWorldclock('#worldclock-utcP8', 8 * 60); // Macao
         //initWorldclock('#worldclock-utcP8', 7 * 60); // Bangkok
@@ -78,7 +78,7 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
                 <span style="position:absolute; font-size: 20px; left: 85px; top: 250px">W&Uuml;RZBURG</span>
                 <span style="position:absolute; font-size: 20px; left: 413px; top: 250px">MACAO</span>
                 <div style="width: 480px; margin: auto; position: relative;">
-                  <span id="worldclock-edt" style="font-size:120px; font-weight: bold; line-height: 100%;"></span>
+                  <span id="worldclock-est" style="font-size:120px; font-weight: bold; line-height: 100%;"></span>
                   <span id="worldclock-cet" style="font-size:50px; font-weight: bold; display: inline-block; margin-top:95px"></span>
                   <span id="worldclock-utcP8" style="font-size:50px; font-weight: bold; display: inline-block; margin-top:95px; position:absolute; right:0;"></span>
                 </div>
